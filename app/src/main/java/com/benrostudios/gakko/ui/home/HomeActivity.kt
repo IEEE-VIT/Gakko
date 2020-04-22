e1f4447c1a7d84813f2a2013d4945839db797134
Vibhor Chinda <vibhorchinda@gmail.com>
2020-04-22 17:36:47 +0530

    added navigation to Home Activity

---
+++ app/src/main/java/com/benrostudios/gakko/ui/home/HomeActivity.kt
@@ -0,0 +1,24 @@
+package com.benrostudios.gakko.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.benrostudios.gakko.R
+import kotlinx.android.synthetic.main.activity_home_activty.*
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_activty)
+
+        navController = findNavController(R.id.nav_host_fragment_home_activity)
+
+        bottom_navigation_view_home_activity.setupWithNavController(navController)
+    }
+}
