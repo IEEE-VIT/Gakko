17073cb3b412070d2edf8d66924752516ff1344e
Vibhor Chinda <vibhorchinda@gmail.com>
2020-05-04 21:08:47 +0530

    pinboard fragment done

--- app/src/main/java/com/benrostudios/gakko/ui/home/pinboard/PinboardFragment.kt
+++ app/src/main/java/com/benrostudios/gakko/ui/home/pinboard/PinboardFragment.kt
@@ -6,12 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.benrostudios.gakko.R
+import com.benrostudios.gakko.adapters.PinboardDisplayAdapter
+import com.benrostudios.gakko.data.models.Material
 import com.benrostudios.gakko.internal.Utils
 import com.benrostudios.gakko.ui.base.ScopedFragment
 import com.benrostudios.gakko.ui.home.material.MaterialFragment
 import kotlinx.android.synthetic.main.pinboard_fragment.*
+import kotlinx.coroutines.launch
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.closestKodein
@@ -21,13 +29,13 @@ class PinboardFragment : ScopedFragment(), KodeinAware {
 
     override val kodein: Kodein by closestKodein()
     private val utils: Utils by instance()
+    private val viewModelFactory: PinboardViewModelFactory by instance()
+    private lateinit var viewModel: PinboardViewModel
+    private val bottomSheetFragment = MaterialFragment()
 
     companion object {
         fun newInstance() = PinboardFragment()
     }
-
-    private val bottomSheetFragment = MaterialFragment()
-    private lateinit var viewModel: PinboardViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,12 +46,13 @@ class PinboardFragment : ScopedFragment(), KodeinAware {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(PinboardViewModel::class.java)
-        // TODO: Use the ViewModel
+        viewModel = ViewModelProvider(this, viewModelFactory).get(PinboardViewModel::class.java)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        getMaterials(utils.retrieveCurrentClassroom()!!)
 
         material_type_assignment_button.setOnClickListener {
             utils.saveMaterialType("Assignment")
@@ -59,6 +68,19 @@ class PinboardFragment : ScopedFragment(), KodeinAware {
             utils.saveMaterialType("Questions")
             bottomSheetFragment.show(activity?.supportFragmentManager!!, bottomSheetFragment.tag)
         }
+    }
 
+    private fun getMaterials(classId: String) = launch {
+        viewModel.getMaterialsList(classId)
+        viewModel.materialsList.observe(viewLifecycleOwner, Observer {
+            updateUI(it)
+        })
+    }
+
+    private fun updateUI(materialsList: List<Material>) {
+        val adapter: PinboardDisplayAdapter = PinboardDisplayAdapter(materialsList)
+        pinboard_recycler_view.adapter = adapter
+        pinboard_recycler_view.addItemDecoration(DividerItemDecoration(requireActivity(), LinearLayout.VERTICAL))
+        pinboard_recycler_view.layoutManager = LinearLayoutManager(requireContext())
     }
 }
