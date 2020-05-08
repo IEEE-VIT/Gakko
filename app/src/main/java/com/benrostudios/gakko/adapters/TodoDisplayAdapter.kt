15157098004613129262b5f6601b09497c0a3bee
Vibhor Chinda <vibhorchinda@gmail.com>
2020-05-08 22:52:56 +0530

    Todo ui done

--- app/src/main/java/com/benrostudios/gakko/adapters/TodoDisplayAdapter.kt
+++ app/src/main/java/com/benrostudios/gakko/adapters/TodoDisplayAdapter.kt
@@ -5,56 +5,65 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
-import androidx.navigation.Navigation
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.benrostudios.gakko.R
-import com.benrostudios.gakko.data.models.Comments
 import com.benrostudios.gakko.data.models.Material
-import com.benrostudios.gakko.data.models.Threads
-import com.benrostudios.gakko.data.models.User
 import com.benrostudios.gakko.internal.GlideApp
-import com.benrostudios.gakko.internal.Utils
-import com.benrostudios.gakko.ui.home.HomeActivity
-import com.bumptech.glide.Glide
-import kotlinx.android.synthetic.main.todo_recycler_list_item.view.*
+import kotlinx.android.synthetic.main.todo_inner_recycler_view_list_item.view.*
 import java.text.SimpleDateFormat
 import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
-class TodoDisplayAdapter(private  val map: HashMap<String, List<Material>>,
-                         private val dateList: List<String>):
+class TodoDisplayAdapter(private val materialList: List<Material>):
     RecyclerView.Adapter<TodoDisplayAdapter.TodoDisplayViewHolder>() {
 
     private lateinit var context: Context
     @SuppressLint("SimpleDateFormat")
+    private var sdf: SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy  hh:mm a")
+    @SuppressLint("SimpleDateFormat")
     private var simpleDateFormat: SimpleDateFormat = SimpleDateFormat("MM  MMMM")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoDisplayViewHolder {
-        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.todo_recycler_list_item, parent, false)
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.todo_inner_recycler_view_list_item, parent, false)
         context = parent.context
         return TodoDisplayViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        return map.size
+        return materialList.size
     }
 
     override fun onBindViewHolder(holder: TodoDisplayViewHolder, position: Int) {
-        holder.date.text = dateList[position]
-        holder.recyclerView.adapter = TodoInnerDisplayAdapter(map[dateList[position]]!!)
-        holder.recyclerView.layoutManager = LinearLayoutManager(context)
-        holder.recyclerView.addItemDecoration(DividerItemDecoration(context, LinearLayout.VERTICAL))
-
+        val material: Material = materialList[position]
+        when (material.type) {
+            "Assignment" -> {
+                GlideApp.with(context)
+                    .load(R.drawable.assignment_vector_icon)
+                    .into(holder.icon)
+            }
+            "Material" -> {
+                GlideApp.with(context)
+                    .load(R.drawable.material_vector_icon)
+                    .into(holder.icon)
+            }
+            else -> {
+                GlideApp.with(context)
+                    .load(R.drawable.quiz_vector_icon)
+                    .into(holder.icon)
+            }
+        }
+        holder.type.text = material.type + " : "
+        holder.title.text = material.name
+        holder.name.text = material.classroomName
+        holder.datePosted.text = "Posted on : " + sdf.format(Date(material.uploadedOn))
+        holder.dueDate.text = "Due Date : " + simpleDateFormat.format(Date(material.deadline * 1000))
     }
 
     class TodoDisplayViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        var date: TextView = view.todo_date_list_item_text_view
-        var recyclerView: RecyclerView = view.todo_list_item_recycler_view
+        val icon = view.todo_inner_list_item_material_icon
+        val type = view.todo_inner_list_item_material_type
+        val title = view.todo_inner_list_item_material_name
+        val datePosted = view.todo_inner_list_item_material_date
+        val name = view.todo_inner_list_item_class_name
+        val dueDate = view.todo_due_date_text_view
     }
 }
--- app/src/main/java/com/benrostudios/gakko/ui/home/todo/TodoFragment.kt
+++ app/src/main/java/com/benrostudios/gakko/ui/home/todo/TodoFragment.kt
@@ -1,20 +1,16 @@
 package com.benrostudios.gakko.ui.home.todo
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.NavController
 import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.benrostudios.gakko.R
 import com.benrostudios.gakko.adapters.TodoDisplayAdapter
+import com.benrostudios.gakko.data.models.Classroom
 import com.benrostudios.gakko.data.models.Material
 import com.benrostudios.gakko.internal.Utils
 import com.benrostudios.gakko.ui.base.ScopedFragment
@@ -39,12 +35,11 @@ class TodoFragment : ScopedFragment(), KodeinAware {
     private lateinit var viewModel: TodoViewModel
     private val utils: Utils by instance()
     private var classIdList = mutableListOf<String>()
+    private var classrooms = mutableListOf<Classroom>()
     private var todoIdList = mutableListOf<String>()
     private var todoList = mutableListOf<Material>()
-    @SuppressLint("SimpleDateFormat")
-    private var simpleDateFormat: SimpleDateFormat = SimpleDateFormat("MM MMMM")
+    private var todoMap: HashMap<String, List<Material>> = HashMap()
     private lateinit var adapter: TodoDisplayAdapter
-    private lateinit var navController: NavController
 
     companion object {
         fun newInstance() = TodoFragment()
@@ -87,7 +82,8 @@ class TodoFragment : ScopedFragment(), KodeinAware {
         todo_fragment_profile_picture.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_todo_to_profileFragment)
         }
-        //getCurrentUser(utils.retrieveMobile()!!)
+        todo_fragment_progress_bar.visibility = View.VISIBLE
+        getCurrentUser(utils.retrieveMobile()!!)
     }
 
     private fun getCurrentUser(userId: String) = launch {
@@ -96,73 +92,92 @@ class TodoFragment : ScopedFragment(), KodeinAware {
             if(!it.classrooms.isNullOrEmpty()) {
                 classIdList = it.classrooms as MutableList<String>
                 getClassrooms(classIdList)
+            } else {
+                defaultUI()
             }
         })
     }
 
-    private fun getClassrooms(classIdList: MutableList<String>) = launch {
-        var counter = 0
+
+
+   private fun getClassrooms(classIdList: List<String>) = launch {
+       classrooms.clear()
+       var counter = 0
+       for(classId: String in classIdList) {
+           viewModel.getClassroom(classId)
+           viewModel.classroom.observe(viewLifecycleOwner, Observer {
+               classrooms.add(it)
+               counter++
+               if(counter == classIdList.size) {
+                   getTodos(classrooms)
+               }
+           })
+       }
+   }
+
+    private fun getTodos(classrooms: List<Classroom>) = launch {
         todoIdList.clear()
-        for(classId: String in classIdList) {
-            viewModel.getClassroom(classId)
-            viewModel.classroom.observe(viewLifecycleOwner, Observer {
-                if(it.createdBy == utils.retrieveMobile()!!) {
-                    todoIdList.add(classId)
-                }
-                counter++
-                if(classIdList.size == counter) {
-                    getTodos(todoIdList)
-                }
-            })
+        todoMap.clear()
+        var flag = 0
+
+        for(classroom: Classroom in classrooms) {
+            if(classroom.createdBy != utils.retrieveMobile()!!) {
+                todoIdList.add(classroom.classroomID)
+            }
+        }
+
+        if(todoIdList.isNotEmpty()) {
+            for (todoId: String in todoIdList) {
+                viewModel.getTodo(todoId)
+                viewModel.todo.observe(viewLifecycleOwner, Observer {
+                    if (it != null) {
+                        todoMap[todoId] = it
+                    }
+                    flag++
+                    if (flag == todoIdList.size) {
+                        if (todoMap.isNotEmpty()) {
+                            updateUI(todoMap)
+                        } else {
+                            defaultUI()
+                        }
+                    }
+                })
+            }
+        } else {
+            defaultUI()
         }
     }
 
-    @SuppressLint("SimpleDateFormat")
-    private fun getTodos(todoIdList: MutableList<String>) = launch {
-        var counter = 0
+    private fun updateUI(todoMap: HashMap<String, List<Material>>) {
         todoList.clear()
-        for(todoId: String in todoIdList) {
-            viewModel.getTodo(todoId)
-            viewModel.todo.observe(viewLifecycleOwner, Observer {
-                for(material: Material in it) {
-                    counter++
-                    val sdf = SimpleDateFormat("dd/MM/yyyy")
-                    val currentDateTime: Date = Calendar.getInstance().time
-                    val currentDate = sdf.parse(sdf.format(currentDateTime))
-                    val dueDate = sdf.parse(sdf.format(Date(material.deadline * 1000)))
+        for (entry: Map.Entry<String, List<Material>> in todoMap.entries) {
 
-                    if (dueDate.after(currentDate)) {
-                        todoList.add(material)
-                    }
+            for (material: Material in entry.value) {
+                val sdf = SimpleDateFormat("dd/MM/yyyy")
+                val currentDateTime: Date = Calendar.getInstance().time
+                val currentDate = sdf.parse(sdf.format(currentDateTime))
+                val dueDate = sdf.parse(sdf.format(Date(material.deadline * 1000)))
 
-                    if (it.size == counter) {
-                        updateUI(todoList)
-                    }
+                if (dueDate.after(currentDate)) {
+                    todoList.add(material)
                 }
-            })
+            }
+        }
+
+        if(todoList.isNotEmpty()) {
+            adapter = TodoDisplayAdapter(todoList)
+            todo_recycler_view.adapter = adapter
+            todo_recycler_view.layoutManager = LinearLayoutManager(requireContext())
+            todo_fragment_progress_bar.visibility = View.GONE
+            todo_recycler_view.visibility = View.VISIBLE
+        } else {
+            defaultUI()
         }
     }
 
-    private fun updateUI(todoList: MutableList<Material>) {
-        val materials = mutableListOf<Material>()
-        val datesList = mutableListOf<String>()
-        val hashMap: HashMap<String, List<Material>> = HashMap()
-
-        for(material: Material in todoList) {
-            materials.clear()
-            for(materialLocal: Material in todoList) {
-                if(simpleDateFormat.format(material.deadline * 1000) == simpleDateFormat.format(materialLocal.deadline * 1000)) {
-                    materials.add(materialLocal)
-                }
-            }
-            hashMap[simpleDateFormat.format(material.deadline * 1000)] = materials
-            if(!datesList.contains(simpleDateFormat.format(material.deadline * 1000))) {
-                datesList.add(simpleDateFormat.format(material.deadline * 1000))
-            }
-        }
-
-        adapter = TodoDisplayAdapter(hashMap, datesList)
-        todo_recycler_view.adapter = adapter
-        todo_recycler_view.layoutManager = LinearLayoutManager(requireContext())
+    private fun defaultUI() {
+        todo_fragment_progress_bar.visibility = View.GONE
+        demo_todo_image_view.visibility = View.VISIBLE
+        demo_todo_text_view.visibility = View.VISIBLE
     }
 }
